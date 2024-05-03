cf7f49df460590fd45c56cdda37f5bf58427c659
zenocoz <federico.soncini@gmail.com>
2024-05-03 20:17:42 +0200

    changed be api for testing

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,15 @@
 import { SingleCryptoInfo } from "../interfaces";
 
+const BACKEND_API = "https://challenge-fullstack.onrender.com";
+
 export const fetchData = async (
 	symbol: string,
 	minutes: number
 ): Promise<SingleCryptoInfo> => {
 	try {
-		const response = await fetch(`/api/price/${symbol}?minutes=${minutes}`);
+		const response = await fetch(
+			`${BACKEND_API}/price/${symbol}?minutes=${minutes}`
+		);
 
 		if (!response.ok) {
 			throw new Error("Failed to fetch data");
